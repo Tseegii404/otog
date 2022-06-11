64b803b4d4f227f4e0d101a8645e7d9ad26df212
serod <od4808@gmail.com>
2022-06-11 14:17:47 +0800

    structure

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className='st'>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
